513db783f08c397ad7eab0b103289d3fd414f66a
Vitaly Dmitriev <vitalik.dmitriev1995@gmail.com>
2018-09-24 22:42:42 +0300

    ready

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  var res = 0;
+  for(var i=1; i<=number; i++){
+    divideonfive(i);
+  }
+
+  function divideonfive(i){
+    if(i%5==0){
+      res++;
+      divideonfive(i/5);
+    }else{
+      return;
+    }
+  }
+  return res;
 }
